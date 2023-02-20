34d0e4005e75cdec66ead3ff2f686fc35b31320b
KathleenN9 <kathleen.govener9@yahoo.com>
2023-02-20 12:21:45 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Task four
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
