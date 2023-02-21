d94211f63758cd958753f2e8148a2ed721a96faa
KathleenN9 <kathleen.govener9@yahoo.com>
2023-02-21 12:45:03 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - print_alphabet_x10 - print the alphabet 10 times
+*/
+void print_alphabet_x10(void)
+
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	for (c ='a'; c <= 'z'; c++);
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+}
